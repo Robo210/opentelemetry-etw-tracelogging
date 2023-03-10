c6653793631f1b544009e4f75ccb86258c1744a4
Kyle Sabo <robo210@gmail.com>
2023-03-10 02:49:49 -0800

    lots o' stuff

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,23 @@
+use opentelemetry::trace::TraceContextExt;
+use opentelemetry::Key;
 use opentelemetry_api::global::shutdown_tracer_provider;
 use opentelemetry_api::trace::Tracer;
-use opentelemetry_etw_tracelogging::*;
+
+const KYLE_KEY: Key = Key::from_static_str("kylesabo.com/foo");
+const SABO_KEY: Key = Key::from_static_str("kylesabo.com/bar");
 
 fn main() {
     let tracer = opentelemetry_etw_tracelogging::new_pipeline()
         .with_name("kyle")
         .install_simple();
     tracer.in_span("doing_work", |cx| {
-        // Traced app logic here...
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+
+        let span = cx.span();
+        span.add_event(
+            "sabo",
+            vec![KYLE_KEY.string("is cool"), SABO_KEY.string("is great")],
+        );
     });
     shutdown_tracer_provider(); // sending remaining spans
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,23 @@
-use opentelemetry::{sdk::export::{
-    trace::{ExportResult, SpanData, SpanExporter},
-    ExportError,
-}, trace::TraceError};
+use chrono::{Datelike, Timelike};
+use opentelemetry::{
+    sdk::export::{
+        trace::{ExportResult, SpanData, SpanExporter},
+        ExportError,
+    },
+    trace::{SpanId, TraceError},
+    Key, Value,
+};
 
 use futures_util::future::BoxFuture;
 use opentelemetry_api::{global, trace::TracerProvider};
 use std::{fmt::Debug};
 
-/// Pipeline builder
 #[derive(Debug)]
 pub struct PipelineBuilder {
     provider_name: String,
-    trace_config: Option<opentelemetry_sdk::trace::Config>
+    trace_config: Option<opentelemetry_sdk::trace::Config>,
 }
 
-/// Create a new stdout exporter pipeline builder.
 pub fn new_pipeline() -> PipelineBuilder {
     PipelineBuilder::default()
 }
@@ -23,13 +26,12 @@ impl Default for PipelineBuilder {
     fn default() -> Self {
         Self {
             provider_name: "TraceLogging-OpenTelemetry".to_owned(),
-            trace_config: None
+            trace_config: None,
         }
     }
 }
 
 impl PipelineBuilder {
-    /// Specify the pretty print setting.
     pub fn with_name(mut self, name: &str) -> Self {
         self.provider_name = name.to_owned();
         self
@@ -42,57 +44,205 @@ impl PipelineBuilder {
     }
 }
 
-impl PipelineBuilder
-{
-    /// Install the stdout exporter pipeline with the recommended defaults.
+impl PipelineBuilder {
     pub fn install_simple(mut self) -> opentelemetry_sdk::trace::Tracer {
         let exporter = Exporter::new(&self.provider_name);
 
-        let mut provider_builder = opentelemetry_sdk::trace::TracerProvider::builder().with_simple_exporter(exporter);
+        let mut provider_builder =
+            opentelemetry_sdk::trace::TracerProvider::builder().with_simple_exporter(exporter);
         if let Some(config) = self.trace_config.take() {
             provider_builder = provider_builder.with_config(config);
         }
 
         let provider = provider_builder.build();
 
-        let tracer = provider.versioned_tracer("opentelemetry-tracelogging", Some(env!("CARGO_PKG_VERSION")), None);
+        let tracer = provider.versioned_tracer(
+            "opentelemetry-tracelogging",
+            Some(env!("CARGO_PKG_VERSION")),
+            None,
+        );
         let _ = global::set_tracer_provider(provider);
 
         tracer
     }
 }
 
+fn add_attributes_to_event2(
+    eb: &mut tracelogging_dynamic::EventBuilder,
+    attribs: &mut dyn Iterator<Item = (&Key, &Value)>,
+) {
+    for attrib in attribs {
+        match attrib.1 {
+            Value::Bool(b) => {
+                eb.add_bool32(
+                    &attrib.0.to_string(),
+                    b.to_owned().into(),
+                    tracelogging_dynamic::OutType::Boolean,
+                    0,
+                );
+            }
+            Value::I64(i) => {
+                eb.add_i64(
+                    &attrib.0.to_string(),
+                    *i,
+                    tracelogging_dynamic::OutType::Signed,
+                    0,
+                );
+            }
+            Value::F64(f) => {
+                eb.add_f64(
+                    &attrib.0.to_string(),
+                    *f,
+                    tracelogging_dynamic::OutType::Signed,
+                    0,
+                );
+            }
+            Value::String(s) => {
+                eb.add_str8(
+                    &attrib.0.to_string(),
+                    &s.to_string(),
+                    tracelogging_dynamic::OutType::String,
+                    0,
+                );
+            }
+            Value::Array(_) => {
+                panic!("go away");
+            }
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Exporter {
-    provider: std::pin::Pin<Box<tracelogging_dynamic::Provider>>
+    provider: std::pin::Pin<Box<tracelogging_dynamic::Provider>>,
 }
 
 impl Exporter {
     pub fn new(provider_name: &str) -> Self {
         let mut provider = Box::pin(tracelogging_dynamic::Provider::new());
         unsafe {
-            provider.as_mut().register(provider_name, &tracelogging_dynamic::Provider::options());
-        }
-        Exporter {
             provider
+                .as_mut()
+                .register(provider_name, &tracelogging_dynamic::Provider::options());
         }
+        Exporter { provider }
     }
 }
 
-impl SpanExporter for Exporter
-{
+impl SpanExporter for Exporter {
     fn export(&mut self, batch: Vec<SpanData>) -> BoxFuture<'static, ExportResult> {
         let mut eb = tracelogging_dynamic::EventBuilder::new();
-        
+
         for span in batch {
-            if self.provider.enabled(tracelogging_dynamic::Level::Informational, 0) {
-                let attribs = format!("{:#?}", span.attributes);
+            if self
+                .provider
+                .enabled(tracelogging_dynamic::Level::Informational, 0)
+            {
+                let activity =
+                    tracelogging_dynamic::Guid::from_name(&span.span_context.span_id().to_string());
+                let parent_activity = if span.parent_span_id == SpanId::INVALID {
+                    None
+                } else {
+                    Some(tracelogging_dynamic::Guid::from_name(
+                        &span.parent_span_id.to_string(),
+                    ))
+                };
+
+                let start_time = chrono::DateTime::from(span.start_time);
+                let start_time_data: [u16; 8] = [
+                    start_time.year() as u16,
+                    start_time.month() as u16,
+                    0,
+                    start_time.day() as u16,
+                    start_time.hour() as u16,
+                    start_time.minute() as u16,
+                    start_time.second() as u16,
+                    (start_time.nanosecond() / 1000000) as u16,
+                ];
+
                 eb.reset(&span.name, tracelogging_dynamic::Level::Informational, 0, 0);
-                eb.add_str8("Attributes", &attribs, tracelogging_dynamic::OutType::Json, 0);
-                let win32err = eb.write(&self.provider, None, None);
+                eb.opcode(tracelogging_dynamic::Opcode::Start);
+                eb.add_systemtime(
+                    "start_time",
+                    &start_time_data,
+                    tracelogging_dynamic::OutType::DateTime,
+                    0,
+                );
+
+                add_attributes_to_event2(&mut eb, &mut span.attributes.iter());
+
+                let mut win32err =
+                    eb.write(&self.provider, Some(&activity), parent_activity.as_ref());
 
                 if win32err != 0 {
-                    return Box::pin(std::future::ready(Err(TraceError::ExportFailed(Box::new(Error{win32err})))));
+                    return Box::pin(std::future::ready(Err(TraceError::ExportFailed(Box::new(
+                        Error { _win32err: win32err },
+                    )))));
+                }
+
+                for event in span.events {
+                    let event_time = chrono::DateTime::from(event.timestamp);
+                    let event_time_data: [u16; 8] = [
+                        event_time.year() as u16,
+                        event_time.month() as u16,
+                        0,
+                        event_time.day() as u16,
+                        event_time.hour() as u16,
+                        event_time.minute() as u16,
+                        event_time.second() as u16,
+                        (event_time.nanosecond() / 1000000) as u16,
+                    ];
+
+                    eb.reset(&event.name, tracelogging_dynamic::Level::Verbose, 0, 0);
+                    eb.opcode(tracelogging_dynamic::Opcode::Info);
+                    eb.add_systemtime(
+                        "time",
+                        &event_time_data,
+                        tracelogging_dynamic::OutType::DateTime,
+                        0,
+                    );
+
+                    add_attributes_to_event2(
+                        &mut eb,
+                        &mut event.attributes.iter().map(|kv| (&kv.key, &kv.value)),
+                    );
+
+                    win32err = eb.write(&self.provider, Some(&activity), parent_activity.as_ref());
+
+                    if win32err != 0 {
+                        return Box::pin(std::future::ready(Err(TraceError::ExportFailed(
+                            Box::new(Error { _win32err: win32err }),
+                        ))));
+                    }
+                }
+
+                let end_time = chrono::DateTime::from(span.end_time);
+                let end_time_data: [u16; 8] = [
+                    end_time.year() as u16,
+                    end_time.month() as u16,
+                    0,
+                    end_time.day() as u16,
+                    end_time.hour() as u16,
+                    end_time.minute() as u16,
+                    end_time.second() as u16,
+                    (end_time.nanosecond() / 1000000) as u16,
+                ];
+
+                eb.reset(&span.name, tracelogging_dynamic::Level::Informational, 0, 0);
+                eb.opcode(tracelogging_dynamic::Opcode::Stop);
+                eb.add_systemtime(
+                    "end_time",
+                    &end_time_data,
+                    tracelogging_dynamic::OutType::DateTime,
+                    0,
+                );
+
+                win32err = eb.write(&self.provider, Some(&activity), parent_activity.as_ref());
+
+                if win32err != 0 {
+                    return Box::pin(std::future::ready(Err(TraceError::ExportFailed(Box::new(
+                        Error { _win32err: win32err },
+                    )))));
                 }
             }
         }
@@ -103,11 +253,11 @@ impl SpanExporter for Exporter
 
 #[derive(Debug)]
 pub struct Error {
-    win32err: u32
+    _win32err: u32,
 }
 
 impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         todo!()
     }
 }
