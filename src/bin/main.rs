7d66787ca361e8ac95cca7a3dd119197ec014538
Kyle Sabo <robo210@gmail.com>
2023-03-10 16:48:46 -0800

    overly complicated wrappers

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,7 @@
 use opentelemetry::trace::TraceContextExt;
 use opentelemetry::Key;
 use opentelemetry_api::global::shutdown_tracer_provider;
-use opentelemetry_api::trace::{Tracer, Span};
+use opentelemetry_api::trace::{Span, Tracer};
 
 const KYLE_KEY: Key = Key::from_static_str("kylesabo.com/foo");
 const SABO_KEY: Key = Key::from_static_str("kylesabo.com/bar");
@@ -9,7 +9,9 @@ const SABO_KEY: Key = Key::from_static_str("kylesabo.com/bar");
 fn main() {
     let tracer = opentelemetry_etw_tracelogging::new_pipeline()
         .with_name("kyle")
+        .with_keyword(2)
         .install_simple();
+
     tracer.in_span("doing_work", |cx| {
         std::thread::sleep(std::time::Duration::from_millis(1000));
 
@@ -20,12 +22,17 @@ fn main() {
         );
     });
 
-    let span_builder = tracer.span_builder("my_cool_span").with_kind(opentelemetry::trace::SpanKind::Client).with_status(opentelemetry::trace::Status::Error { description: "asdf".into() });
+    let span_builder = tracer
+        .span_builder("my_cool_span")
+        .with_kind(opentelemetry::trace::SpanKind::Client)
+        .with_status(opentelemetry::trace::Status::Error {
+            description: "asdf".into(),
+        });
     let mut span = span_builder.start(&tracer);
     std::thread::sleep(std::time::Duration::from_millis(1000));
     span.add_event("qwerty", vec![]);
     std::thread::sleep(std::time::Duration::from_millis(1000));
-    drop(span);
 
+    drop(span);
     shutdown_tracer_provider(); // sending remaining spans
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,19 @@ use opentelemetry::{
         trace::{ExportResult, SpanData, SpanExporter},
         ExportError,
     },
-    trace::{SpanId, TraceError, SpanKind, Status},
+    trace::{SpanId, SpanKind, Status, TraceError},
     Key, Value,
 };
 
 use futures_util::future::BoxFuture;
 use opentelemetry_api::{global, trace::TracerProvider};
-use tracelogging_dynamic::*;
 use std::fmt::Debug;
+use tracelogging_dynamic::*;
 
 #[derive(Debug)]
 pub struct PipelineBuilder {
     provider_name: String,
+    keyword: u64,
     trace_config: Option<opentelemetry_sdk::trace::Config>,
 }
 
@@ -27,6 +28,7 @@ impl Default for PipelineBuilder {
     fn default() -> Self {
         Self {
             provider_name: "TraceLogging-OpenTelemetry".to_owned(),
+            keyword: 1,
             trace_config: None,
         }
     }
@@ -35,6 +37,11 @@ impl Default for PipelineBuilder {
 impl PipelineBuilder {
     pub fn with_name(mut self, name: &str) -> Self {
         self.provider_name = name.to_owned();
+        self
+    }
+
+    pub fn with_keyword(mut self, keyword: u64) -> Self {
+        self.keyword = keyword;
         self
     }
 
@@ -83,28 +90,13 @@ fn add_attributes_to_event(
                 );
             }
             Value::I64(i) => {
-                eb.add_i64(
-                    &attrib.0.to_string(),
-                    *i,
-                    OutType::Signed,
-                    0,
-                );
+                eb.add_i64(&attrib.0.to_string(), *i, OutType::Signed, 0);
             }
             Value::F64(f) => {
-                eb.add_f64(
-                    &attrib.0.to_string(),
-                    *f,
-                    OutType::Signed,
-                    0,
-                );
+                eb.add_f64(&attrib.0.to_string(), *f, OutType::Signed, 0);
             }
             Value::String(s) => {
-                eb.add_str8(
-                    &attrib.0.to_string(),
-                    &s.to_string(),
-                    OutType::Utf8,
-                    0,
-                );
+                eb.add_str8(&attrib.0.to_string(), &s.to_string(), OutType::Utf8, 0);
             }
             Value::Array(_) => {
                 panic!("go away");
@@ -113,9 +105,79 @@ fn add_attributes_to_event(
     }
 }
 
+struct Win32SystemTime {
+    st: [u16; 8],
+}
+
+impl From<std::time::SystemTime> for Win32SystemTime {
+    fn from(value: std::time::SystemTime) -> Self {
+        let dt = chrono::DateTime::from(value);
+
+        Win32SystemTime {
+            st: [
+                dt.year() as u16,
+                dt.month() as u16,
+                0,
+                dt.day() as u16,
+                dt.hour() as u16,
+                dt.minute() as u16,
+                dt.second() as u16,
+                (dt.nanosecond() / 1000000) as u16,
+            ],
+        }
+    }
+}
+
+struct EventBuilderWrapper {
+    eb: EventBuilder,
+}
+
+impl EventBuilderWrapper {
+    fn add_win32_systemtime(
+        &mut self,
+        field_name: &str,
+        win32_systemtime: &Win32SystemTime,
+        field_tag: u32,
+    ) -> &mut Self {
+        self.eb.add_systemtime(
+            field_name,
+            &win32_systemtime.st,
+            OutType::DateTime,
+            field_tag,
+        );
+        self
+    }
+
+    fn add_string(
+        &mut self,
+        field_name: &str,
+        field_value: impl AsRef<[u8]>,
+        field_tag: u32,
+    ) -> &mut Self {
+        self.eb
+            .add_str8(field_name, field_value, OutType::Utf8, field_tag);
+        self
+    }
+}
+
+impl std::ops::Deref for EventBuilderWrapper {
+    type Target = EventBuilder;
+    fn deref(&self) -> &Self::Target {
+        &self.eb
+    }
+}
+
+impl std::ops::DerefMut for EventBuilderWrapper {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.eb
+    }
+}
+
 #[derive(Debug)]
 pub struct Exporter {
     provider: std::pin::Pin<Box<Provider>>,
+    span_keywords: u64,
+    event_keywords: u64,
 }
 
 impl Exporter {
@@ -126,57 +188,41 @@ impl Exporter {
                 .as_mut()
                 .register(provider_name, &Provider::options());
         }
-        Exporter { provider }
+        Exporter {
+            provider,
+            span_keywords: 1,
+            event_keywords: 2,
+        }
     }
 }
 
 impl SpanExporter for Exporter {
     fn export(&mut self, batch: Vec<SpanData>) -> BoxFuture<'static, ExportResult> {
-        let mut eb = EventBuilder::new();
+        let mut ebw = EventBuilderWrapper {
+            eb: EventBuilder::new(),
+        };
 
         for span in batch {
-            if self
-                .provider
-                .enabled(Level::Informational, 0)
-            {
-                let activity =
-                    Guid::from_name(&span.span_context.span_id().to_string());
+            // We probably want to allow more fine-grained control over the keywords in the future
+            let (level, keyword) = match span.status {
+                Status::Ok => (Level::Informational, self.span_keywords),
+                Status::Error { .. } => (Level::Error, self.span_keywords),
+                Status::Unset => (Level::Verbose, self.span_keywords),
+            };
+
+            if self.provider.enabled(level, keyword) {
+                let activity = Guid::from_name(&span.span_context.span_id().to_string());
                 let parent_activity = if span.parent_span_id == SpanId::INVALID {
                     None
                 } else {
-                    Some(Guid::from_name(
-                        &span.parent_span_id.to_string(),
-                    ))
+                    Some(Guid::from_name(&span.parent_span_id.to_string()))
                 };
 
-                let start_time = chrono::DateTime::from(span.start_time);
-                let start_time_data: [u16; 8] = [
-                    start_time.year() as u16,
-                    start_time.month() as u16,
-                    0,
-                    start_time.day() as u16,
-                    start_time.hour() as u16,
-                    start_time.minute() as u16,
-                    start_time.second() as u16,
-                    (start_time.nanosecond() / 1000000) as u16,
-                ];
+                ebw.eb.reset(&span.name, level, keyword, 0);
+                ebw.eb.opcode(Opcode::Start);
+                ebw.add_win32_systemtime("start_time", &span.start_time.into(), 0);
 
-                let level = match span.status {
-                    Status::Ok => Level::Informational,
-                    Status::Error{..} => Level::Error,
-                    Status::Unset => Level::Verbose,
-                };
-
-                eb.reset(&span.name, level, 0, 0);
-                eb.opcode(Opcode::Start);
-                eb.add_systemtime(
-                    "start_time",
-                    &start_time_data,
-                    OutType::DateTime,
-                    0,
-                );
-
-                eb.add_str8(
+                ebw.add_string(
                     "span_kind",
                     match span.span_kind {
                         SpanKind::Client => "Client",
@@ -185,93 +231,62 @@ impl SpanExporter for Exporter {
                         SpanKind::Consumer => "Consumer",
                         SpanKind::Internal => "Internal",
                     },
-                    OutType::Utf8,
                     0,
                 );
 
-                if let Status::Error{description} = span.status {
-                    eb.add_str8("error", description.to_string(), OutType::Utf8, 0);
+                if let Status::Error { description } = span.status {
+                    ebw.add_string("error", description.to_string(), 0);
                 };
 
-                add_attributes_to_event(&mut eb, &mut span.attributes.iter());
+                add_attributes_to_event(&mut ebw, &mut span.attributes.iter());
 
                 let mut win32err =
-                    eb.write(&self.provider, Some(&activity), parent_activity.as_ref());
+                    ebw.eb
+                        .write(&self.provider, Some(&activity), parent_activity.as_ref());
 
                 if win32err != 0 {
                     return Box::pin(std::future::ready(Err(TraceError::ExportFailed(Box::new(
-                        Error {
-                            _win32err: win32err,
-                        },
+                        Error { win32err },
                     )))));
                 }
 
-                for event in span.events {
-                    let event_time = chrono::DateTime::from(event.timestamp);
-                    let event_time_data: [u16; 8] = [
-                        event_time.year() as u16,
-                        event_time.month() as u16,
-                        0,
-                        event_time.day() as u16,
-                        event_time.hour() as u16,
-                        event_time.minute() as u16,
-                        event_time.second() as u16,
-                        (event_time.nanosecond() / 1000000) as u16,
-                    ];
+                if self.provider.enabled(Level::Verbose, self.event_keywords) {
+                    for event in span.events {
+                        ebw.eb
+                            .reset(&event.name, Level::Verbose, self.event_keywords, 0);
+                        ebw.eb.opcode(Opcode::Info);
 
-                    eb.reset(&event.name, Level::Verbose, 0, 0);
-                    eb.opcode(Opcode::Info);
-                    eb.add_systemtime(
-                        "time",
-                        &event_time_data,
-                        OutType::DateTime,
-                        0,
-                    );
+                        ebw.add_win32_systemtime("time", &event.timestamp.into(), 0);
 
-                    add_attributes_to_event(
-                        &mut eb,
-                        &mut event.attributes.iter().map(|kv| (&kv.key, &kv.value)),
-                    );
+                        add_attributes_to_event(
+                            &mut ebw,
+                            &mut event.attributes.iter().map(|kv| (&kv.key, &kv.value)),
+                        );
 
-                    win32err = eb.write(&self.provider, Some(&activity), parent_activity.as_ref());
+                        win32err =
+                            ebw.eb
+                                .write(&self.provider, Some(&activity), parent_activity.as_ref());
 
-                    if win32err != 0 {
-                        return Box::pin(std::future::ready(Err(TraceError::ExportFailed(
-                            Box::new(Error {
-                                _win32err: win32err,
-                            }),
-                        ))));
+                        if win32err != 0 {
+                            return Box::pin(std::future::ready(Err(TraceError::ExportFailed(
+                                Box::new(Error { win32err }),
+                            ))));
+                        }
                     }
                 }
 
-                let end_time = chrono::DateTime::from(span.end_time);
-                let end_time_data: [u16; 8] = [
-                    end_time.year() as u16,
-                    end_time.month() as u16,
-                    0,
-                    end_time.day() as u16,
-                    end_time.hour() as u16,
-                    end_time.minute() as u16,
-                    end_time.second() as u16,
-                    (end_time.nanosecond() / 1000000) as u16,
-                ];
+                ebw.eb.reset(&span.name, level, keyword, 0);
+                ebw.eb.opcode(Opcode::Stop);
 
-                eb.reset(&span.name, Level::Informational, 0, 0);
-                eb.opcode(Opcode::Stop);
-                eb.add_systemtime(
-                    "end_time",
-                    &end_time_data,
-                    OutType::DateTime,
-                    0,
-                );
+                ebw.add_win32_systemtime("end_time", &span.end_time.into(), 0);
 
-                win32err = eb.write(&self.provider, Some(&activity), parent_activity.as_ref());
+                win32err = ebw
+                    .eb
+                    .write(&self.provider, Some(&activity), parent_activity.as_ref());
 
                 if win32err != 0 {
                     return Box::pin(std::future::ready(Err(TraceError::ExportFailed(Box::new(
-                        Error {
-                            _win32err: win32err,
-                        },
+                        Error { win32err },
                     )))));
                 }
             }
@@ -283,12 +298,12 @@ impl SpanExporter for Exporter {
 
 #[derive(Debug)]
 pub struct Error {
-    _win32err: u32,
+    win32err: u32,
 }
 
 impl std::fmt::Display for Error {
-    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("Win32 error: {}", self.win32err))
     }
 }
 impl std::error::Error for Error {}
